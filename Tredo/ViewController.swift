7cda2fe7c559c7a873dd2ecb6ea259a798c2d352
Alexander Lee <genius.lees@gmail.com>
2021-12-25 16:36:01 +0600

    Initial Commit

---
+++ Tredo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tredo
+//
+//  Created by Alexander Lee on 25.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
